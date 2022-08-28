1644daeca49fcd99bc9d040fc65e7f00d9753d31
Federico Errica <f.errica@protonmail.com>
2022-08-28 22:33:52 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@ set -eax
 
 interrogate --version
 
-if interrogate --fail-under $2 --generate-badge $3 $1 | grep -q 'PASSED'; then
-    interrogate --fail-under $2 --generate-badge $3 $1
+if interrogate -i -M --fail-under $2 --generate-badge $3 $1 | grep -q 'PASSED'; then
+    interrogate -i -M --fail-under $2 --generate-badge $3 $1
     exit 0
 else 
-    interrogate --fail-under $2 --generate-badge $3 $1
+    interrogate -i -M --fail-under $2 --generate-badge $3 $1
     exit 1
 fi
